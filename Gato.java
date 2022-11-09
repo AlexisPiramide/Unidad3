a02aefe825cf571cffa942418c931c84dd495996
Alexis Torres <220240@fppiramide.com>
2022-11-09 10:16:07 +0100

    Gato

---
+++ Gato.java
@@ -0,0 +1,15 @@
+public class Gato {
+
+    private String Nombre;
+    private String Raza;
+    private String Color;
+    private Boolean Vivo;
+    private  String Tamaño;
+    private int Edad;
+    private int TazasRotas;
+    private int NumeroDeVecesDiariosQueLeMiraALosOjosAdiosYSeRieEnSuCara;
+    private int PactosConElDiablo;
+    private double Miausporminuto;
+    private int CrimenesDeGuerraCometidos;
+
+}
---
+++ UsarGato.java
@@ -0,0 +1,7 @@
+public class UsarGato {
+
+    public static void main(String[] args) {
+        Gato migato = new Gato ();
+
+    }
+}
