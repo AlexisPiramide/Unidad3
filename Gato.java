e846cb16d682ad043a6638c4cab2ab104a4b2fcf
Alexis Torres <220240@fppiramide.com>
2022-11-10 12:36:31 +0100

    Gato

--- Gato.java
+++ Gato.java
@@ -1,3 +1,6 @@
+import javax.sound.midi.Soundbank;
+import java.util.SortedMap;
+
 public class Gato {
 
     private String Nombre;
@@ -8,8 +11,54 @@ public class Gato {
     private int Edad;
     private int TazasRotas;
     private int NumeroDeVecesDiariosQueLeMiraALosOjosAdiosYSeRieEnSuCara;
-    private int PactosConElDiablo;
+    int PactosConElDiablo;
     private double Miausporminuto;
     private int CrimenesDeGuerraCometidos;
 
+    
+    
+    Gato(String nombre,String raza,String color,boolean vivo,int edad,int tazasRotas,int ndvdqlmaloaysresc, int pactosConElDiablo,int miausporminuto, int crimenesDeGuerraCometidos){
+        this.Nombre=nombre;
+        this.Raza=raza;
+        this.Color=color;
+        this.Vivo=vivo;
+        this.Edad=edad;
+        this.TazasRotas=tazasRotas;
+        this.NumeroDeVecesDiariosQueLeMiraALosOjosAdiosYSeRieEnSuCara=ndvdqlmaloaysresc;
+        this.PactosConElDiablo=pactosConElDiablo;
+        this.Miausporminuto=miausporminuto;
+        this.CrimenesDeGuerraCometidos=crimenesDeGuerraCometidos;
+    }
+@Override
+    public String toString(){
+
+        String salida;
+        salida= String.format("Nombre: %s\tRaza: %s\tColor: %s\tVivo: %b\nEdad: %d\nTazas rotas: %d\nNº de veces diarias que a mirado a Dios a los ojos y se ha reido de el en su cara: %d\nPactos con el diablo: %d\tMiaus Por Minuto: %f\tCrimenes de guerra: %d",Nombre,Raza,Color,Vivo,Edad,TazasRotas,NumeroDeVecesDiariosQueLeMiraALosOjosAdiosYSeRieEnSuCara,PactosConElDiablo,Miausporminuto,CrimenesDeGuerraCometidos);
+
+        return salida;
+    }
+    public boolean vivo(){
+
+       if (this.Vivo==false)
+        return false;
+
+        return true;
+    }
+    public void revive(){
+        this.Vivo=true;
+        this.PactosConElDiablo=0;
+    }
+
+    public void matagato(){
+        this.Vivo=false;
+        System.out.println("El gato a muerto");
+    }
+    public void tazas1(){
+        this.TazasRotas++;
+        System.out.println("Tazas rotas: "+TazasRotas);
+    }
+    public void pactos(){
+        this.PactosConElDiablo++;
+        System.out.println("Pactos con el diablo hechos: "+PactosConElDiablo);
+    }
 }
--- UsarGato.java
+++ UsarGato.java
@@ -1,7 +1,74 @@
+import java.util.Scanner;
+
 public class UsarGato {
 
     public static void main(String[] args) {
-        Gato migato = new Gato ();
+        Gato migato = new Gato("Marramiau", "Caniche", "Negro", true, 9, 104, 3, 5, 5, 8);
+        System.out.println(migato);
 
+        int Lasaña;
+        Scanner sc = new Scanner(System.in);
+
+        int contador = 0;
+        int lasaña = 0;
+        int x = 1;
+        boolean estado = migato.vivo();
+        while (x==1){
+        while (estado) {
+            System.out.println("----------------------------");
+            System.out.println("¿Que hace el gato?");
+            System.out.println("----------------------------");
+            System.out.println("1º Lamerse las bolas");
+            System.out.println("2º Tirar una taza");
+            System.out.println("3º Comer Lasaña");
+            System.out.println("4º Hacer un pacto con el diablo");
+            System.out.println("5º Salir");
+            contador = sc.nextInt();
+
+            if (contador == 1) {
+                System.out.println("El gato se lame las bolas");
+            }
+            if (contador == 2) {
+                System.out.println("El gato ha tirado 1 taza");
+                migato.tazas1();
+
+            }
+            if (contador == 3) {
+                System.out.println("El gato se ha comido una lasaña");
+                lasaña++;
+                if (lasaña == 13) {
+                    migato.matagato();
+                    estado = migato.vivo();
+                }
+
+            }
+            if (contador == 4) {
+                System.out.println("El gato a hecho un pacto con el diablo");
+                migato.pactos();
+                if (migato.PactosConElDiablo == 9) {
+                    migato.matagato();
+                    contador = 5;
+                    estado = migato.vivo();
+                }
+            }
+        }
+        if (contador == 5) x = 0;
+
+        while (!estado) {
+            System.out.println("----------------------------");
+            System.out.println("¿Que hacer con el gato muerto?");
+            System.out.println("----------------------------");
+            System.out.println("1º Revivirlo");
+            System.out.println("2º Irse");
+            contador = sc.nextInt();
+            if (contador == 1) {
+                migato.revive();
+                lasaña = 0;
+            }
+            if (contador == 2) {
+                x = 0;
+            }
+        }
+    }
     }
 }
